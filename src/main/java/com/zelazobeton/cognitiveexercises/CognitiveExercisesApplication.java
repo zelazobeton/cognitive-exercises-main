1120131d93d95ece62fdb2ee77907680db536dc6
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2020-09-26 09:18:52 +0200

    Initial commit

---
+++ src/main/java/com/zelazobeton/cognitiveexercises/CognitiveExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.zelazobeton.cognitiveexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zelazobeton/cognitiveexercises/CognitiveExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelazobeton.cognitiveexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiveExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
