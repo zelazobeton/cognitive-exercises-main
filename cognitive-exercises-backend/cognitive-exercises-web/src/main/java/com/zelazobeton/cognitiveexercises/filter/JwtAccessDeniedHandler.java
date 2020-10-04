6e7c32dce8402bf1e1e687f1dfbe47ba97021e19
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2020-10-04 10:42:31 +0200

    BE: Refactor authentication filters

--- cognitive-exercises-backend/cognitive-exercises-web/src/main/java/com/zelazobeton/cognitiveexercises/filter/JwtAccessDeniedHandler.java
+++ cognitive-exercises-backend/cognitive-exercises-web/src/main/java/com/zelazobeton/cognitiveexercises/filter/JwtAccessDeniedHandler.java
@@ -2,10 +2,8 @@ package com.zelazobeton.cognitiveexercises.filter;
 
 import static com.zelazobeton.cognitiveexercises.constant.SecurityConstants.ACCESS_DENIED_MSG;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +12,17 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zelazobeton.cognitiveexercises.HttpResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+    private final JwtServletRequestHandler jwtServletRequestHandler;
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException {
-        HttpResponse httpResponse = new HttpResponse(UNAUTHORIZED, ACCESS_DENIED_MSG);
-        response.setContentType(APPLICATION_JSON_VALUE);
-        response.setStatus(UNAUTHORIZED.value());
-        OutputStream outputStream = response.getOutputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(outputStream, httpResponse);
-        outputStream.flush();
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception)
+            throws IOException {
+        jwtServletRequestHandler.handle(request, response, UNAUTHORIZED, ACCESS_DENIED_MSG);
     }
 }
--- cognitive-exercises-backend/cognitive-exercises-web/src/main/java/com/zelazobeton/cognitiveexercises/filter/JwtAuthenticationEntryPoint.java
+++ cognitive-exercises-backend/cognitive-exercises-web/src/main/java/com/zelazobeton/cognitiveexercises/filter/JwtAuthenticationEntryPoint.java
@@ -2,10 +2,8 @@ package com.zelazobeton.cognitiveexercises.filter;
 
 import static com.zelazobeton.cognitiveexercises.constant.SecurityConstants.FORBIDDEN_MSG;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +12,17 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zelazobeton.cognitiveexercises.HttpResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
+    private final JwtServletRequestHandler jwtServletRequestHandler;
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
-        HttpResponse httpResponse = new HttpResponse(FORBIDDEN, FORBIDDEN_MSG);
-        response.setContentType(APPLICATION_JSON_VALUE);
-        response.setStatus(FORBIDDEN.value());
-        OutputStream outputStream = response.getOutputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(outputStream, httpResponse);
-        outputStream.flush();
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException {
+        jwtServletRequestHandler.handle(request, response, FORBIDDEN, FORBIDDEN_MSG);
     }
 }
