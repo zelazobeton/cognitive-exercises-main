8a4d3b2556e73b8a17d0119eaf3828bceb070fac
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2021-04-16 17:34:42 +0200

    FE: Move setting language functionality to service

--- cognitive-exercises-frontend/src/app/app.component.ts
+++ cognitive-exercises-frontend/src/app/app.component.ts
@@ -1,36 +1,14 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslationService} from './shared/service/translation.service';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import {TranslateService} from '@ngx-translate/core';
-import {Subscription} from 'rxjs';
-import {LocaleDto} from './shared/model/locale-dto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
-  private subscriptions: Subscription[] = [];
+export class AppComponent {
 
-  constructor(private translationService: TranslationService, private translate: TranslateService) {
-    translate.addLangs(['en', 'es']);
-    translate.setDefaultLang('en');
-    this.subscriptions.push(
-      this.translationService.getLocale().subscribe(
-        (response: LocaleDto) => {
-          translate.langs.find(lang => lang === response.language) === undefined
-            ? translate.use('en')
-            : translate.use(response.languageTag);
-        },
-        (errorResponse: HttpErrorResponse) => {
-          translate.setDefaultLang('en');
-        }
-      )
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+  constructor(private translationService: TranslationService) {
+    translationService.setLanguage();
   }
 }
--- cognitive-exercises-frontend/src/app/shared/service/translation.service.ts
+++ cognitive-exercises-frontend/src/app/shared/service/translation.service.ts
@@ -1,38 +1,41 @@
 import {Injectable, OnDestroy} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {Observable, Subscription} from 'rxjs';
-import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {LocaleDto} from '../model/locale-dto';
 
 @Injectable()
-export class TranslationService {
-  //private subscriptions: Subscription[] = [];
+export class TranslationService implements OnDestroy {
+  private subscriptions: Subscription[] = [];
   private readonly host = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
-  //constructor(private http: HttpClient, public translate: TranslateService) {
-  //  translate.addLangs(['en', 'es']);
-  //  this.subscriptions.push(
-  //    this.getLocale().subscribe(
-  //      (response) => {
-  //        console.log(response);
-  //        translate.langs.find(lang => lang === response) === undefined
-  //          ? translate.setDefaultLang('en')
-  //          : translate.setDefaultLang(response);
-  //      },
-  //      (errorResponse: HttpErrorResponse) => {
-  //        translate.setDefaultLang('en');
-  //      }
-  //    )
-  //  );
-  //}
+  //constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private translate: TranslateService) {
+  }
+
+  public setLanguage() {
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+    this.subscriptions.push(
+      this.getLocale().subscribe(
+        (response: LocaleDto) => {
+          this.translate.langs.find(lang => lang === response.language) === undefined
+            ? this.translate.use('en')
+            : this.translate.use(response.languageTag);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.translate.setDefaultLang('en');
+        }
+      )
+    );
+  }
 
   public getLocale(): Observable<LocaleDto> {
     return this.http.get<LocaleDto>(`${this.host}/lang/locale`);
   }
 
-  //ngOnDestroy(): void {
-  //  this.subscriptions.forEach(sub => sub.unsubscribe());
-  //}
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
