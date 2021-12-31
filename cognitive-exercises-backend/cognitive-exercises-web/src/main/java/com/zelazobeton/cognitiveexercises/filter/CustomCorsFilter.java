8ac32c5c229935997081bab94d5e10339e373aab
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2021-12-31 16:13:49 +0100

    BE: Move auth to keycloak pt.3 	* Add filter to prevent CORS error for keycloak error response 	  after unsuccessful authentication

---
+++ cognitive-exercises-backend/cognitive-exercises-web/src/main/java/com/zelazobeton/cognitiveexercises/filter/CustomCorsFilter.java
@@ -0,0 +1,37 @@
+package com.zelazobeton.cognitiveexercises.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CustomCorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse res,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
